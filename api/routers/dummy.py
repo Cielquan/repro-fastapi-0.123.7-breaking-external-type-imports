b188758e69eacbfe311bb5b2c69959c303b58178
Christian Riedel <christian.riedel@siegwerk.com>
2025-12-10 09:23:00 +0100

    remove logger

--- api/routers/dummy.py
+++ api/routers/dummy.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-import logging
 import typing as t
 
 import fastapi
 
 if t.TYPE_CHECKING:
     import config
-
-
-logger = logging.getLogger("app")
 
 
 router = fastapi.APIRouter(tags=["Dummy"], prefix="/dummy")
